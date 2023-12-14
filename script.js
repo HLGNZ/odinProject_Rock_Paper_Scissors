e044ee7ad1af89220fe44c7ba79ff3f83ef49402
Hasan <hmee202016@gmail.com>
2023-12-14 16:04:16 +0300

    Add: playRound function and call it
    - playRound function with if condition made comparison
    
    -  return who wins, tie or lose in the game
    
    $ pseudocode
    
    - If playerSelection equles computerSelection is tie
    - If Rock beats Scissors you win
    - If Scissors beats Paper you win
    - If Paper beats Rock you win
    - Else you lose
    - Made playerSelection string to case-insensitive
    - Console.log who wins, tie or lose to test

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-console.log(getComputerChoice())
 // getComputerChoice function reutern Rock, Paper and Scissors randomly.
 function getComputerChoice() {
   let choice = ["Rock", "Paper", "Scissors"]
   let random = parseInt(Math.random() * choice.length)
   return choice[random]
 }
+// playRound function rutern who Wins, Tie or Lose in the game.
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return `You Tie ${playerSelection} with ${computerSelection}.`
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return `You Win ${playerSelection} beats ${computerSelection}.`
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return `You Win ${playerSelection} beats ${computerSelection}.`
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return `You Win ${playerSelection} beats ${computerSelection}.`
+  } else {
+    return `You Lose ${playerSelection} doesn't beat ${computerSelection}`
+  }
+}
+// massage alert user can input value.
+const playerSelection = prompt("Enter your choice Rock, Paper or Scissors")
+const computerSelection = getComputerChoice()
+console.log(
+  playRound(
+    // Switch first letter to captial then switch all letters to lower case
+    playerSelection[0].toUpperCase() + playerSelection.toLowerCase().slice(1),
+    computerSelection
+  )
+)
